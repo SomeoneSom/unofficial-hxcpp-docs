9bc929264fa7d12e758c99beadb66d9158f3560b
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2022-10-29 19:12:52 -0400

    tocdepth test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-#html_theme_options = {
-#    'navigation_depth': -1,
-#    'collapse-navigation': False
-#}
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': -1,
+    'collapse-navigation': False
+}
 html_static_path = ['_static']
