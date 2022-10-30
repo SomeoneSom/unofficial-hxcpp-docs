ad0e136abb524e959c5b961a185440f3e1b54afb
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2022-10-29 22:37:48 -0400

    oops messed up theme name!

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx-rtd-theme'
+html_theme = 'sphinx_rtd_theme'
 html_css_files = [
     'css/rtd_dark.css',
 ]
